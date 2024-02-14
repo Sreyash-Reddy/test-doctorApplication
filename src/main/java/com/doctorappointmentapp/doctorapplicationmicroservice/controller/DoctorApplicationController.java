078d13075a3c19068b3d876b530bbd22108d8f68
SREYASH REDDY <sreyashreddy221@gmail.com>
2024-02-14 20:07:16 +0530

    Removed Controller for Doctor login

--- src/main/java/com/doctorappointmentapp/doctorapplicationmicroservice/controller/DoctorApplicationController.java
+++ src/main/java/com/doctorappointmentapp/doctorapplicationmicroservice/controller/DoctorApplicationController.java
@@ -1,10 +1,7 @@
 package com.doctorappointmentapp.doctorapplicationmicroservice.controller;
 
-import com.doctorappointmentapp.doctorapplicationmicroservice.entities.Doctor;
 import com.doctorappointmentapp.doctorapplicationmicroservice.service.DoctorApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +10,4 @@ public class DoctorApplicationController {
     @Autowired
     private DoctorApplicationService doctorApplicationService;
 
-    @PostMapping("login/doctor")
-    public Boolean loginDoctor(@RequestBody String Username){
-        return true;
-    }
 }
