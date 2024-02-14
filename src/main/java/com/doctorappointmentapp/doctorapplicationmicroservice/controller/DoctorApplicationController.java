41f4ca3c8c18d72454606fe98f922910a6524bf4
SREYASH REDDY <sreyashreddy221@gmail.com>
2024-02-14 19:36:10 +0530

    Added Controller for Doctor login

--- src/main/java/com/doctorappointmentapp/doctorapplicationmicroservice/controller/DoctorApplicationController.java
+++ src/main/java/com/doctorappointmentapp/doctorapplicationmicroservice/controller/DoctorApplicationController.java
@@ -1,7 +1,10 @@
 package com.doctorappointmentapp.doctorapplicationmicroservice.controller;
 
+import com.doctorappointmentapp.doctorapplicationmicroservice.entities.Doctor;
 import com.doctorappointmentapp.doctorapplicationmicroservice.service.DoctorApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +12,9 @@ public class DoctorApplicationController {
 
     @Autowired
     private DoctorApplicationService doctorApplicationService;
+
+    @PostMapping("login/doctor")
+    public Boolean loginDoctor(@RequestBody String Username){
+        return true;
+    }
 }
