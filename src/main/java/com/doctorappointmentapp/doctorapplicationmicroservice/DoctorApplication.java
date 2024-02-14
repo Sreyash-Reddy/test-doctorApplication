0758871f6be38a9ad9a724f14569ae3032f75ea9
SREYASH REDDY <sreyashreddy221@gmail.com>
2024-02-14 17:56:51 +0530

    first commit

---
+++ src/main/java/com/doctorappointmentapp/doctorapplicationmicroservice/DoctorApplication.java
@@ -0,0 +1,13 @@
+package com.doctorappointmentapp.doctorapplicationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorApplication.class, args);
+	}
+
+}
